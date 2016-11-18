ba4448897d03b6bfe426207aab4020039178b2d2
Francis Chong <francis@ignition.hk>
2016-11-18 15:34:59 +0800

    update porject setting

---
+++ ObjectiveLevelDB/ObjectiveLevelDB/ObjectiveLevelDB.h
@@ -0,0 +1,19 @@
+//
+//  ObjectiveLevelDB.h
+//  ObjectiveLevelDB
+//
+//  Created by Chan Fai Chong on 18/11/2016.
+//  Copyright © 2016 Time Based Technology Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjectiveLevelDB.
+FOUNDATION_EXPORT double ObjectiveLevelDBVersionNumber;
+
+//! Project version string for ObjectiveLevelDB.
+FOUNDATION_EXPORT const unsigned char ObjectiveLevelDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveLevelDB/PublicHeader.h>
+
+
---
+++ ObjectiveLevelDB/ObjectiveLevelDB_OSX/ObjectiveLevelDB_OSX.h
@@ -0,0 +1,19 @@
+//
+//  ObjectiveLevelDB_OSX.h
+//  ObjectiveLevelDB_OSX
+//
+//  Created by Chan Fai Chong on 18/11/2016.
+//  Copyright © 2016 Time Based Technology Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ObjectiveLevelDB_OSX.
+FOUNDATION_EXPORT double ObjectiveLevelDB_OSXVersionNumber;
+
+//! Project version string for ObjectiveLevelDB_OSX.
+FOUNDATION_EXPORT const unsigned char ObjectiveLevelDB_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveLevelDB_OSX/PublicHeader.h>
+
+
